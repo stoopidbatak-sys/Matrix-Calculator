d3061c377a4e16927f294d115bc3e94f236bd93f
stoopidbatak-sys <stoopidbatak@gmail.com>
2026-05-02 00:48:39 +0500

    Added Main, Fixed Memory Leak and a few small fixes

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Matrix_utilities.h"
+
+int main() {
+    MainMenu();
+
+    int choice = InputChoice();
+    Operations Task = InputEnum(choice);
+    SwitchBody(Task);
+
+    while (Task != Operations :: Exit) {
+        std::cout<<"\n===Perform Another Task==="<<std::endl;
+        Features();
+        choice = InputChoice();
+        Task = InputEnum(choice);
+        SwitchBody(Task);
+    }
+
+    return 0;
+}
